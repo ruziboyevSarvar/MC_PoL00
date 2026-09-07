168a676fae96d57b97c9b8ef73c060a7401e6439
Ruziboyev Sarvar <sarvarruziboyev244@gmail.com>
2026-09-07 09:51:01 +0500

    Initial commit_2

--- McPoLOO_backend/docker-entrypoint.sh
+++ McPoLOO_backend/docker-entrypoint.sh
@@ -6,7 +6,18 @@ if [ "${DATABASE_URL:-}" != "" ]; then
     postgres://*|postgresql://*)
       db_url="${DATABASE_URL#postgres://}"
       db_url="${db_url#postgresql://}"
-      db_host_path="${db_url#*@}"
+      if [ "${db_url#*@}" != "$db_url" ]; then
+        db_credentials="${db_url%%@*}"
+        db_host_path="${db_url#*@}"
+        if [ "${db_credentials#*:}" != "$db_credentials" ]; then
+          export DATABASE_USERNAME="${DATABASE_USERNAME:-${db_credentials%%:*}}"
+          export DATABASE_PASSWORD="${DATABASE_PASSWORD:-${db_credentials#*:}}"
+        else
+          export DATABASE_USERNAME="${DATABASE_USERNAME:-$db_credentials}"
+        fi
+      else
+        db_host_path="$db_url"
+      fi
       export DATABASE_URL="jdbc:postgresql://${db_host_path}"
       ;;
   esac
